903e4df078a3cd05e317e6c2df71cc744b5b3614
Turbotard <turbotard9@gmail.com>
2024-05-13 14:18:28 +0200

    Add hourly rate page with navigation buttons and Vercel logo

---
+++ src/app/hourlyrate/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"; 
+import NavigationButtons from "../../components/allbutton";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hourly Rates</h1>
+      <NavigationButtons/>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={72}
+        height={16}
+      />
+    </div>
+  );
+}
