bb5e600ec425af36a13f088c3064860f7b242583
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2024-05-16 16:34:00 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
 "use client";
+import React, { useEffect } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useEffect } from "react";
-import SearchForm from '../components/SearchForm';
+import SearchForm from "../components/SearchForm";
 import { LoadingSpinner } from "@/components/LoadingSpinner/LoadingSpinner";
-import Dashboard from './dashboard';
-import HistoriqueBesoins from '../components/historiqueDesBesoins';
-import FormulaireBesoin from '@/components/formulaireBesoin';
+import Dashboard from "./dashboard";
 
 function Home() {
   const { data: session, status } = useSession();
+
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'development' && status === "unauthenticated") {
+    if (process.env.NODE_ENV !== "development" && status === "unauthenticated") {
       signIn("keycloak", {
         callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
       }); // Force sign in if not authenticated
       //!!! ROLES ARE IN session.user.roles when authenticated !!!
     }
-  }, [session]);
-  if (process.env.NODE_ENV !== 'development' && status === "unauthenticated")
+  }, [session, status]);
+
+  if (process.env.NODE_ENV !== "development" && status === "unauthenticated") {
     return (
       <div>
         <LoadingSpinner />
       </div>
     );
+  }
+
   return (
     <div className={"flex flex-col"}>
       <button onClick={() => signOut()}>Sign out</button>
       {/* welcome to home{" "} */}
       <Dashboard />
+      {/* Composant SearchForm */}
+      <SearchForm />
     </div>
   );
 }
