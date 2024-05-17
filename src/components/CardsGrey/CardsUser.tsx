54eacfccbd1076a06719ce474cb49a35238d9c25
Adilina04 <113524418+Adilina04@users.noreply.github.com>
2024-05-17 15:18:19 +0200

    style

--- src/components/CardsGrey/CardsUser.tsx
+++ src/components/CardsGrey/CardsUser.tsx
@@ -16,9 +16,8 @@ const CardsGrey: React.FC<CardData> = ({ name, hourlyrate, status }) => {
         <p className="text-sm text-gray-600">Taux horaire souhaité : {hourlyrate}</p>
       </div>
       <div className="p-4">
-        <span className={`inline-block text-sm px-3 py-1 rounded-full ${status === 'En attente' ? 'text-yellow-600 bg-yellow-200' : 'text-green-600 bg-green-200'}`}>
-          {status}
-        </span>
+      <span className={`inline-block text-sm px-3 py-1 rounded-full ${status === 'En attente' ? 'text-yellow-600 bg-yellow-200' : 'text-green-600 bg-green-200'} ${status === 'Rejeté' ? 'text-red-600 bg-red-200' : 'text-green-600 bg-green-200'} `}>{status}
+</span>
       </div>
       <div className="p-4">
         <button className="text-blue-800 hover:bg-blue-800 hover:text-white rounded px-4 py-2 transition-colors duration-200">
@@ -30,21 +29,27 @@ const CardsGrey: React.FC<CardData> = ({ name, hourlyrate, status }) => {
 };
 
 const Dashboard: React.FC = () => {
-  const [cardsData, setCardsData] = useState<CardData[]>([]);
+  // const [cardsData, setCardsData] = useState<CardData[]>([]);
+  const [cardsData, setCardsData] = useState<CardData[]>([
+    { id: 1, name: 'François CORNET', hourlyrate: '50€/h', status: 'En attente' },
+    { id: 2, name: 'Robin PENEA', hourlyrate: '70€/h', status: 'Approuvé' },
+    { id: 3, name: 'lloana', hourlyrate: '65€/h', status: 'Rejeté' },
+  ])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('https://3000'); // Remplacez l'URL par votre endpoint réel
+  //       const data: CardData[] = response.data;
+  //       setCardsData(data);
+  //     } catch (error) {
+  //       console.error('erreur :', error);
+  //     }
+  //   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://3000'); // Remplacez l'URL par votre endpoint réel
-        const data: CardData[] = response.data;
-        setCardsData(data);
-      } catch (error) {
-        console.error('erreur :', error);
-      }
-    };
+  //   fetchData();
+  // }, []);
 
-    fetchData();
-  }, []);
+
 
   return (
     <div>
@@ -56,3 +61,4 @@ const Dashboard: React.FC = () => {
 };
 
 export default Dashboard;
+
