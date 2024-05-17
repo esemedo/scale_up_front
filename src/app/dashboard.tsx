b9aa02cc570201f33c73c918c03e10959aae07ea
Daadaa Nabil <103863031+RyzKum@users.noreply.github.com>
2024-05-17 12:07:13 +0200

    adding style

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,28 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import SubjectsTab from '../components/TabDashboard/SubjectTab';
-import PromotionTab from '../components/TabDashboard/PromotionTab';
-import NeedsTab from '@/components/TabDashboard/NeedsTab';
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SubjectsTab from "../components/TabDashboard/SubjectTab";
+import PromotionTab from "../components/TabDashboard/PromotionTab";
+import NeedsTab from "@/components/TabDashboard/NeedsTab";
 
 const Dashboard = () => {
-    const [activeTab, setActiveTab] = useState('promotions');
+  const [activeTab, setActiveTab] = useState("promotions");
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen ">
-            <div className="p-8 bg-white rounded-lg shadow-md w-3/4" style={{ height: '700px' }}>
-                <div className="mb-4 flex ">
-                    <button onClick={() => setActiveTab('promotions')} className={`flex-grow text-center py-2 ${activeTab === 'promotions' ? 'border-b-2 border-blue-500' : ''}`}>Nos Promotions</button>
-                    <button onClick={() => setActiveTab('subjects')} className={`flex-grow text-center py-2 ${activeTab === 'subjects' ? 'border-b-2 border-blue-500' : ''}`}>Nos Matières</button>
-                    <button onClick={() => setActiveTab('needs')} className={`flex-grow text-center py-2 ${activeTab === 'needs' ? 'border-b-2 border-blue-500' : ''}`}>Nos Besoins</button>
-                </div>
-                {activeTab === 'promotions' && <PromotionTab />}
-                {activeTab === 'subjects' && <SubjectsTab />}
-                {activeTab === 'needs' &&  <NeedsTab />}
-            </div>
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center ">
+                <div className="rounded p-5 text-center text-4xl font-bold mb-5 text-blue-500">
+          DASHBOARD
         </div>
-    );
+      <div
+        className="w-3/4 rounded-lg bg-white p-8 shadow-md h-[700px]" 
+      >
+        <div className="mb-4 flex ">
+          <button
+            onClick={() => setActiveTab("promotions")}
+            className={`flex-grow py-2 text-center ${
+              activeTab === "promotions" ? "border-b-2 border-blue-500" : ""
+            }`}
+          >
+            Nos Promotions
+          </button>
+          <button
+            onClick={() => setActiveTab("subjects")}
+            className={`flex-grow py-2 text-center ${
+              activeTab === "subjects" ? "border-b-2 border-blue-500" : ""
+            }`}
+          >
+            Nos Matières
+          </button>
+          <button
+            onClick={() => setActiveTab("needs")}
+            className={`flex-grow py-2 text-center ${
+              activeTab === "needs" ? "border-b-2 border-blue-500" : ""
+            }`}
+          >
+            Nos Besoins
+          </button>
+        </div>
+        {activeTab === "promotions" && <PromotionTab />}
+        {activeTab === "subjects" && <SubjectsTab />}
+        {activeTab === "needs" && <NeedsTab />}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/components/formulaireBesoin.tsx
+++ src/components/formulaireBesoin.tsx
@@ -1,150 +1,221 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { MultiSelect } from 'react-multi-select-component';
-import Alert from '@mui/material/Alert';
-import { useSession } from 'next-auth/react';
-const api = process.env.NEXT_PUBLIC_API_URL;
-
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { MultiSelect } from "react-multi-select-component";
+import Alert from "@mui/material/Alert";
 
 const FormulaireBesoin: React.FC = () => {
+  const [promotions, setPromotions] = useState<any[]>([]);
+  const [contributors, setContributors] = useState<any[]>([]);
+  const [subjects, setSubjects] = useState<any[]>([]);
+  const [selectedSubjects, setSelectedSubjects] = useState<any[]>([]);
+  const api = process.env.NEXT_PUBLIC_API_URL;
+  const [alert, setAlert] = useState<JSX.Element | null>(null);
 
-    const [promotions, setPromotions] = useState<any[]>([]);
-    const [contributors, setContributors] = useState<any[]>([]);
-    const [subjects, setSubjects] = useState<any[]>([]);
-    const [selectedSubjects, setSelectedSubjects] = useState<any[]>([]);
-    const api = process.env.NEXT_PUBLIC_API_URL;
-    const [alert, setAlert] = useState<JSX.Element | null>(null);
-    const { data: session  } = useSession();
-
-
-    const fetchPromotions = async () => {
-        try {
-            const response = await axios.get<any[]>(`${api}/api/promotions`,{headers:{Authorization: `Bearer ${session?.accessToken}`}});
-            setPromotions(response.data);
-        } catch (error) {
-            console.error('Error fetching promotions:', error);
-        }
+  const fetchPromotions = async () => {
+    try {
+      const response = await axios.get<any[]>(`${api}/api/promotions`);
+      setPromotions(response.data);
+    } catch (error) {
+      console.error("Error fetching promotions:", error);
     }
+  };
 
-    const fetchContributors = async () => {
-        try {
-            const response = await axios.get<any[]>(`${api}/api/contributors`,{headers:{Authorization: `Bearer ${session?.accessToken}`}});
-            setContributors(response.data);
-        } catch (error) {
-            console.error('Error fetching contributors:', error);
-        }
+  const fetchContributors = async () => {
+    try {
+      const response = await axios.get<any[]>(`${api}/api/contributors`);
+      setContributors(response.data);
+    } catch (error) {
+      console.error("Error fetching contributors:", error);
     }
+  };
 
-    const fetchSubjects = async () => {
-        try {
-            const response = await axios.get<any[]>(`${api}/api/subjects`,{headers:{Authorization: `Bearer ${session?.accessToken}`}});
-            setSubjects(response.data);
-        } catch (error) {
-            console.error('Error fetching subjects:', error);
-        }
+  const fetchSubjects = async () => {
+    try {
+      const response = await axios.get<any[]>(`${api}/api/subjects`);
+      setSubjects(response.data);
+    } catch (error) {
+      console.error("Error fetching subjects:", error);
     }
+  };
 
-    useEffect(() => {
-        if (session){
-        fetchPromotions();
-        fetchContributors();
-        fetchSubjects();
-        }
-        
-    }, [session]);
+  useEffect(() => {
+    fetchPromotions();
+    fetchContributors();
+    fetchSubjects();
+  }, []);
 
-    const handleSubmit = function(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const form = event.currentTarget;
-        const promotion = form['promotion'].value;
-        const subjects = selectedSubjects.map((subject) => subject.value);
-        const contributor = form['contributor'].value;
-        const startSchoolYear = form['startSchoolYear'].value;
-        const endSchoolYear = form['endSchoolYear'].value;
-        const hours = selectedSubjects.map((subject) => parseInt(form['hoursVolume_' + subject.label].value));
-        const totalHours = hours.reduce((total, current) => total + current, 0);
-
-        axios.post(`${api}/api/needs`, {
-            "promotion": { "connect": { "id": parseInt(promotion) } },
-            "status": 1,
-            "contributor": { "connect": { "id": parseInt(contributor) } },
-            "startSchoolYear": parseInt(startSchoolYear),
-            "endSchoolYear": parseInt(endSchoolYear),
-            "subject": { "connect": subjects.map((subject) => ({ "id": parseInt(subject) })) },
-            "hoursVolume": totalHours
-        },{headers:{Authorization: `Bearer ${session?.accessToken}`}}).then((response) => {
-            console.log('Need created:', response.data);
-            if (response.status === 201) {
-                const alert = <Alert severity="success" onClose={() => {setAlert(null)}} className='mb-4 absolute z-50 top-1'>Le besoin a été créé avec succès</Alert>;
-                setAlert(alert);
-            }
-        }).catch((error) => {
-            console.error('Error creating need:', error);
-            const alert = <Alert severity="error" onClose={() => {setAlert(null)}} className='mb-4 absolute z-50 top-1'>Erreur lors de la création du besoin</Alert>;
-            setAlert(alert);
-        });
-    }
-
-
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <form onSubmit={handleSubmit} className='bg-white p-8 rounded-lg border border-gray-300 shadow-lg flex flex-col gap-4 w-96'>
-                <div>
-                    <label htmlFor='promotion' className='mr-2'>Promotion :</label>
-                    <select id='promotion' className='border rounded-md px-2 py-1 w-full' required  >
-                        {promotions.map((promotion) => (
-                            <option key={promotion.id} value={promotion.id}>
-                                {promotion.startSchoolYear}-{promotion.endSchoolYear}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-
-                <div>
-                    <label htmlFor='subjects' className='mr-2'>Matières :</label>
-                    <MultiSelect 
-                    options={subjects.map((subject) => ({ label: subject.name, value: subject.id }))}
-                    value={selectedSubjects}
-                    onChange={setSelectedSubjects}
-                    labelledBy="Sélectionnez les matières"
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor='contributor' className='mr-2'>Contributeur :</label>
-                    <select id='contributor' className='border rounded-md px-2 py-1 w-full' required>
-                        {contributors.map((contributor) => (
-                            <option key={contributor.id} value={contributor.id}>
-                                {contributor.firstName} {contributor.lastName}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-
-                <div>
-                    <label htmlFor='startSchoolYear' className='mr-2'>Année scolaire de début :</label>
-                    <input type='number' id='startSchoolYear' className='border rounded-md px-2 py-1 w-full' required />
-                </div>
-
-                <div>
-                    <label htmlFor='endSchoolYear' className='mr-2'>Année scolaire de fin :</label>
-                    <input type='number' id='endSchoolYear' className='border rounded-md px-2 py-1 w-full' required />
-                </div>
-
-                {selectedSubjects ? selectedSubjects.map((subject) => (
-                    <div key={subject.value}>
-                        <label htmlFor={"hoursVolume_" + subject.label} className='mr-2'>Volume horaire pour la matière {subject.label} :</label>
-                        <input type='number' id={"hoursVolume_" + subject.label} className='border rounded-md px-2 py-1 w-full' required />
-                    </div>
-                )) : null}
-
-                <button type='submit' className='bg-blue-500 text-white px-4 py-2 rounded-md button'>Créer le besoin</button>
-            </form>
-
-            {alert}
-        </div>
+  const handleSubmit = function (event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const promotion = form["promotion"].value;
+    const subjects = selectedSubjects.map((subject) => subject.value);
+    const contributor = form["contributor"].value;
+    const startSchoolYear = form["startSchoolYear"].value;
+    const endSchoolYear = form["endSchoolYear"].value;
+    const hours = selectedSubjects.map((subject) =>
+      parseInt(form["hoursVolume_" + subject.label].value),
     );
-}
+    const totalHours = hours.reduce((total, current) => total + current, 0);
+
+    axios
+      .post(`${api}/api/needs`, {
+        promotion: { connect: { id: parseInt(promotion) } },
+        status: 1,
+        contributor: { connect: { id: parseInt(contributor) } },
+        startSchoolYear: parseInt(startSchoolYear),
+        endSchoolYear: parseInt(endSchoolYear),
+        subject: {
+          connect: subjects.map((subject) => ({ id: parseInt(subject) })),
+        },
+        hoursVolume: totalHours,
+      })
+      .then((response) => {
+        console.log("Need created:", response.data);
+        if (response.status === 201) {
+          const alert = (
+            <Alert
+              severity="success"
+              onClose={() => {
+                setAlert(null);
+              }}
+              className="absolute top-1 z-50 mb-4"
+            >
+              Le besoin a été créé avec succès
+            </Alert>
+          );
+          setAlert(alert);
+        }
+      })
+      .catch((error) => {
+        console.error("Error creating need:", error);
+        const alert = (
+          <Alert
+            severity="error"
+            onClose={() => {
+              setAlert(null);
+            }}
+            className="absolute top-1 z-50 mb-4"
+          >
+            Erreur lors de la création du besoin
+          </Alert>
+        );
+        setAlert(alert);
+      });
+  };
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center ">
+      <div className="mb-5 rounded p-5 text-center text-4xl font-bold text-blue-500">
+        Formulaire de déclaration de besoin
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-96 flex-col gap-4 rounded-lg border border-gray-300 bg-white p-8 shadow-lg"
+      >
+        <div>
+          <label htmlFor="promotion" className="mr-2">
+            Promotion :
+          </label>
+          <select
+            id="promotion"
+            className="w-full rounded-md border px-2 py-1"
+            required
+          >
+            {promotions.map((promotion) => (
+              <option key={promotion.id} value={promotion.id}>
+                {promotion.startSchoolYear}-{promotion.endSchoolYear}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="subjects" className="mr-2">
+            Matières :
+          </label>
+          <MultiSelect
+            options={subjects.map((subject) => ({
+              label: subject.name,
+              value: subject.id,
+            }))}
+            value={selectedSubjects}
+            onChange={setSelectedSubjects}
+            labelledBy="Sélectionnez les matières"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="contributor" className="mr-2">
+            Contributeur :
+          </label>
+          <select
+            id="contributor"
+            className="w-full rounded-md border px-2 py-1"
+            required
+          >
+            {contributors.map((contributor) => (
+              <option key={contributor.id} value={contributor.id}>
+                {contributor.firstName} {contributor.lastName}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="startSchoolYear" className="mr-2">
+            Année scolaire de début :
+          </label>
+          <input
+            type="number"
+            id="startSchoolYear"
+            className="w-full rounded-md border px-2 py-1"
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="endSchoolYear" className="mr-2">
+            Année scolaire de fin :
+          </label>
+          <input
+            type="number"
+            id="endSchoolYear"
+            className="w-full rounded-md border px-2 py-1"
+            required
+          />
+        </div>
+
+        {selectedSubjects
+          ? selectedSubjects.map((subject) => (
+              <div key={subject.value}>
+                <label
+                  htmlFor={"hoursVolume_" + subject.label}
+                  className="mr-2"
+                >
+                  Volume horaire pour la matière {subject.label} :
+                </label>
+                <input
+                  type="number"
+                  id={"hoursVolume_" + subject.label}
+                  className="w-full rounded-md border px-2 py-1"
+                  required
+                />
+              </div>
+            ))
+          : null}
+
+        <button
+          type="submit"
+          className="button rounded-md bg-blue-500 px-4 py-2 text-white"
+        >
+          Créer le besoin
+        </button>
+      </form>
+
+      {alert}
+    </div>
+  );
+};
 
 export default FormulaireBesoin;
